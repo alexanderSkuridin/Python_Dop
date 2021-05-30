316b3630f15df38961685b54f4dff634ac7e7518
Alexander <55065762+alexanderSkuridin@users.noreply.github.com>
2021-05-30 20:35:37 +0300

    Add files via upload

---
+++ practice03_dop/part1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="study_package",
+    version="0.0.1",
+    author="Alexander",
+    author_email="skuridin.a.s@edu.mirea.ru",
+    packages=["study_package"],
+    description="This is a test package for python practice"
+)
